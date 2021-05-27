0b7feff690c3146df0db8f180ee6d8bc1b597647
Dong Giang <hodonggiangst2@gmail.com>
2021-05-27 15:22:35 +0700

    Draw sql

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/insert',function(){
+    DB::insert('insert into posts(title,content) value (?,?)', ['tieu de 2','noi dung dong Thu 2']);   
+});
+
+Route::get("/read",function(){
+    $results = DB::select("select * from posts");
+    // print_r($results->title);
+    foreach($results as $post){
+        echo $post->title;
+        echo '<p>' . $post->content.'</p>';
+    }
+});
+
+Route::get('/update',function(){
+    $result = DB::update('update posts set title = "title update 1" Where id = ?',[1]);
+    return $result;
 });
